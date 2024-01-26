d0b2a36fea9da58ca8c0d08ab48b3019e013804b
npatsatsia <npatsatsia1@gmail.com>
2024-01-26 14:23:51 +0300

    Update useRefreshToken.js

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -1,36 +1,26 @@
-import useAuth from "./useAuth";
-import api from '../api/axios';
+import useAuth from "./useAuth"
+import api from '../api/axios'
 
 const useRefreshToken = () => {
-    const { setAuth } = useAuth();
+    const {setAuth} = useAuth() 
 
     const refresh = async () => {
-        try {
-            const response = await api.get('/refresh', {
-                withCredentials: true,
-            });
-
-            // Update authentication state
-            setAuth(prevAuth => ({
-                ...prevAuth,
-                role: response.data.role,
-                jwt: response.data.accessToken,
+        const response = await api.get('/refresh', {
+            withCredentials: true,
+        });
+        setAuth(prev => {
+            console.log(JSON.stringify(prev))
+            console.log(response.data.accessToken)
+            return {...prev, 
+                role: response.data.role, 
+                jwt: response.data.accessToken, 
                 username: response.data.username
-            }));
+            }
+        })
+        return response.data.accessToken
+    }
 
-            console.log("Authentication state after refresh:", JSON.stringify(prevAuth));
-            console.log("New access token:", response.data.accessToken);
-
-            return response.data.accessToken;
-        } catch (error) {
-            // Handle error or log appropriately
-            console.error("Error refreshing token:", error);
-            // Throw the error to propagate it further if needed
-            throw error;
-        }
-    };
-
-    return refresh;
+  return refresh
 }
 
-export default useRefreshToken;
+export default useRefreshToken
